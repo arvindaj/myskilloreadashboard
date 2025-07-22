7000d5b00f8161027de10f480cc0d0555dfb6e93
arvindj <jeevaarvind489@gmail.com>
2025-07-22 23:01:59 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    open: true
+  },
+  build: {
+    outDir: 'dist'
+  },
+  // For Vercel & SPA support
+  base: '/',
 })
